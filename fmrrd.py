880665f81d676f8afee4f2474e3f81d4469dc65f
Stanislaw Klekot <dozzie@jarowit.net>
2013-04-01 00:43:58 +0200

    Dispensed with non-blocking I/O for RRD frontend.
    It's simpler with blocking reads.

--- fmrrd.py
+++ fmrrd.py
@@ -1,36 +1,9 @@
 #!/usr/bin/python
 
 import sys
-import time
 import rrdtool
+import json
 
-#-----------------------------------------------------------------------------
-# non-blocking I/O {{{
-
-def set_nonblocking(filehandle):
-  from os import O_NONBLOCK
-  from fcntl import fcntl, F_SETFL, F_GETFL
-  fd = filehandle.fileno()
-  fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK)
-
-def try_read_json(filehandle):
-  import errno
-  import json
-  result = None
-
-  try:
-    line = filehandle.readline()
-    if line != '':
-      result = json.loads(line)
-  except IOError, e:
-    if e.errno == errno.EAGAIN:
-      pass
-    else:
-      raise e
-
-  return result
-
-# }}}
 #-----------------------------------------------------------------------------
 
 if len(sys.argv) < 2:
@@ -42,22 +15,20 @@ if len(sys.argv) < 2:
 
 rrd_file = sys.argv[1]
 
-set_nonblocking(sys.stdin)
-
 try:
   while True:
-    stream = try_read_json(sys.stdin)
-    while stream != None:
-      if stream['stream_id'] == 0:
-        data = '%(time)d:%(bytes)d:%(packets)d' % (stream)
-        print "updating stream %d: %s" % (stream['stream_id'], data)
-        rrdtool.update(rrd_file, data)
-      else:
-        print "ignored stream %d" % (stream['stream_id'])
-      stream = try_read_json(sys.stdin)
+    line = sys.stdin.readline()
+    if line == '':
+      break
+    stream = json.loads(line)
 
-    # no stream data left -- wait for more
-    time.sleep(0.01)
+    if stream['stream_id'] == 0:
+      data = '%(time)d:%(bytes)d:%(packets)d' % (stream)
+      print "updating stream %d: %s" % (stream['stream_id'], data)
+      rrdtool.update(rrd_file, data)
+    else:
+      print "ignored stream %d" % (stream['stream_id'])
+
 except KeyboardInterrupt:
   pass
 
